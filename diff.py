9b6cc168dd950b230b0ba2b74be2a2606aca50a9
German M. Bravo <german.mb@deipi.com>
2013-02-17 11:43:48 -0600

    Added --follow to diffs and added revision numbers

--- diff.py
+++ diff.py
@@ -6,13 +6,56 @@ from .git import git_root, GitTextCommand, GitWindowCommand, do_when, goto_xy
 
 class GitDiff (object):
     def run(self, edit=None, ignore_whitespace=False):
-        command = ['git', 'diff', '--no-color']
+        command = ['git', 'log', '--name-only', '--pretty=%s\a%h %an <%aE>\a%ad (%ar)',
+            '--date=local', '--max-count=9000']
         if ignore_whitespace:
             command.extend(('--ignore-all-space', '--ignore-blank-lines'))
-        command.extend(('--', self.get_file_name()))
-        self.run_command(command, self.diff_done)
+        file_name = self.get_file_name()
+        if file_name:
+            command.extend(['--follow', '--', file_name])
+        self.run_command(
+            command,
+            self.show_done)
 
-    def diff_done(self, result):
+    def show_done(self, result):
+        # GitLog Copy-Past
+        import os
+        self.results = []
+        self.files = {}
+        relative = None
+        for r in result.strip().split('\n'):
+            if r:
+                _result = r.strip().split('\a', 2)
+                if len(_result) == 1:
+                    if relative is None:
+                        # Find relative path
+                        relative = os.sep.join(['..'] * (len(os.path.normpath(_result[0]).split(os.sep)) - 1))
+                        if relative:
+                            relative += os.sep
+                    ref = result[1].split(' ', 1)[0]
+                    self.files[ref] = relative + _result[0]
+                else:
+                    result = _result
+                    ref = result[1].split(' ', 1)
+                    result[0] = u"%s - %s" % (ref[0], result[0])
+                    result[1] = ref[1]
+                    self.results.append(result)
+        self.quick_panel(self.results, self.panel_done)
+
+    def panel_done(self, picked):
+        if 0 > picked < len(self.results):
+            return
+        item = self.results[picked]
+        # the commit hash is the first thing on the second line
+        ref = item[0].split(' ', 1)[0]
+        command = ['git', 'diff', '-C', '--no-color', ref, '--']
+        command.extend(set(self.files.values() + [self.get_file_name()]))
+        self.run_command(
+            command,
+            self.diff_done,
+            ref=ref)
+
+    def diff_done(self, result, ref):
         if not result.strip():
             self.panel("No output")
             return
@@ -36,7 +79,7 @@ class GitDiff (object):
 
 class GitDiffCommit (object):
     def run(self, edit=None, ignore_whitespace=False):
-        command = ['git', 'diff', '--cached', '--no-color']
+        command = ['git', 'diff', '-C', '--no-color', '--cached']
         if ignore_whitespace:
             command.extend(('--ignore-all-space', '--ignore-blank-lines'))
         self.run_command(command, self.diff_done)
